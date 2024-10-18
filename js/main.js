6403b82f3a275f9a1d115ae257c6dc7eade5407b
Armstrong Omoregie <ammycan6@gmail.com>
2024-10-18 13:18:15 +0100

    Build v1.8

--- js/main.js
+++ js/main.js
@@ -1,23 +1,8 @@
-/*  ---------------------------------------------------
-    Template Name: Manup
-    Description: Manup Event HTML Template
-    Author: Colorlib
-    Author URI: http://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
 
 'use strict';
 
 (function ($) {
 
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
 
     /*------------------
         Background Set
@@ -35,34 +20,7 @@
         allowParentLinks: true
     });
 
-    /*------------------------
-		Partner Slider
-    ----------------------- */
-    $(".partner-logo").owlCarousel({
-        items: 6,
-        dots: false,
-        autoplay: true,
-        loop: true,
-        smartSpeed: 1200,
-        margin: 116,
-        responsive: {
-            320: {
-                items: 2,
-            },
-            480: {
-                items: 3,
-            },
-            768: {
-                items: 4,
-            },
-            992: {
-                items: 5,
-            },
-            1200: {
-                items: 6
-            }
-        }
-    });
+    
 
     /*------------------------
 		Testimonial Slider
